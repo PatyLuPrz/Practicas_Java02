c21f36aff556b91f8e13195c6bc8357b94d2b0f4
Patricia Pérez Martínez <37462546+PatyLuPrz@users.noreply.github.com>
2018-07-02 01:43:45 -0500

    Add files via upload

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+package holamundo;
+public class HolaMundo 
+{
+    private int x;
+    public static void main(String[] args) 
+    {
+        System.out.println("hola k ase");
+    }    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
